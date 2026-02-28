7e278f71f386f6f09f6760d64d83596e422f35e7
Vuong <vuongdvv03@gmail.com>
2026-02-28 20:45:57 +0700

    second commit

--- config/vnpay.php
+++ config/vnpay.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'vnp_TmnCode'    => 'CT3UDZ9Z',
-    'vnp_HashSecret' => 'KJCME2TWQTWI4KVDO3T0WZ8MFUFI1TDD',
+    'vnp_TmnCode'    => 'Your TmnCode',
+    'vnp_HashSecret' => 'Your HashSecret',
     'vnp_Url'        => 'https://sandbox.vnpayment.vn/paymentv2/vpcpay.html',
     'vnp_ReturnUrl'  => 'https://unvertical-multigranulated-kathline.ngrok-free.dev/laptop-shop/payment/vnpay_return.php',
     'vnp_IpnUrl'     => 'https://unvertical-multigranulated-kathline.ngrok-free.dev/laptop-shop/payment/vnpay_ipn.php',
